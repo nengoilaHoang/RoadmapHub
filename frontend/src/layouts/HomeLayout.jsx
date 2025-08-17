7ee99680260270e4dd395fe42a493eaf6808338f
KienLoveNH <kientri098@gmail.com>
2025-08-17 09:51:50 +0700

    create layout

---
+++ frontend/src/layouts/HomeLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import NavBar from '#components/Home/NavBar.jsx';
+import Footer from '#components/Home/Footer.jsx';
+export default function HomeLayout(){
+    return (
+        <div className="d-flex flex-column min-vh-100">
+            <NavBar />
+            <main className="flex-fill container my-4">
+                <h1>Welcome to Home Layout</h1>
+            <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
+}
