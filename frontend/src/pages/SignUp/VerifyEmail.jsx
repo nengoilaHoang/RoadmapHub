efd9de61fce41ffd29aa60a9ea44e5bd79b18cd2
KienLoveNH <kientri098@gmail.com>
2025-08-17 09:54:14 +0700

    create pages signup and verify email

---
+++ frontend/src/pages/SignUp/VerifyEmail.jsx
@@ -0,0 +1,14 @@
+export default function VerifyEmail(){
+    return(
+    <>
+    <div className="d-flex flex-column justify-content-center align-items-center mt-5">
+        <img src=""/>
+        <h1>
+            Verify your email address
+        </h1>
+        <p>We have sent you an email at <b>22110046@student.hcmute.edu.vn.</b> Please click the link to verify your account. This link will expire shortly, so please verify soon!</p>
+        <p>Please make sure to check your spam folder. If you still don't have the email click to <a href="#">resend verification email.</a></p>
+    </div>
+
+    </>)
+}
