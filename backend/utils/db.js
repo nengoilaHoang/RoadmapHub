1d069cb3c0d4f8c22c64ce8211f4040c0a543aca
KienLoveNH <kientri098@gmail.com>
2025-08-18 23:31:33 +0700

    update db

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,11 +2,11 @@ import knex from 'knex'
 export default knex({
     client:'mysql2',
     connection:{
-        host:'127.0.0.1',
+        host:'localhost',
         port:3306,
         user:'root',
         password:'',
-        database:'Login'
+        database:'roadmap'
 
 
     },
