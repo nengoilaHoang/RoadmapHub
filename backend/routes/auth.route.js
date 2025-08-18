b859924c338f311acac18af02477f0cd7e9fa6cf
KienLoveNH <kientri098@gmail.com>
2025-08-18 23:35:21 +0700

    update auth.route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -3,4 +3,7 @@ import AccountController from '../controllers/Account.controller.js';
 const router = express.Router();
 
 router.post('/login',AccountController.login);
+router.post('/verify-email',AccountController.verifyEmail);
+router.get('/verify/:token',AccountController.verify);
+router.post('/signup-google',AccountController.signUpGoogle)
 export default router;
