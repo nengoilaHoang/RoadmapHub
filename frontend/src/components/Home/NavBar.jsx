5cd1dce85e3631586073d3fa786ee5e9248e12a7
nengoilaHoang <levanviethoang04@gmail.com>
2025-08-22 18:00:13 +0700

    feature: logout

--- frontend/src/components/Home/NavBar.jsx
+++ frontend/src/components/Home/NavBar.jsx
@@ -1,5 +1,62 @@
-
+import { useCheckLogin } from "../../hooks/userCheckLogin"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import './home.css';
 export default function NavBar() {
+    const navigate = useNavigate();
+    const { isLoggedIn } = useCheckLogin();
+    function onLogin() {navigate('/login')}
+    function onSignup() {navigate('/signup')}
+    async function onLogout() {
+        await axios.post(`http://localhost:5000/api/accounts/logout`,{}, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            withCredentials: true
+        });
+        navigate('/login');
+    }
+    let dropdown;
+    if (!isLoggedIn) {
+        dropdown = 
+        <div className="auth-buttons">
+            <button className="btn login-btn" onClick={onLogin}>Login</button>
+            <button className="btn logout-btn" onClick={onSignup}>sign up</button>
+        </div>
+    }
+    else{
+        dropdown = 
+        <div className="dropdown">
+        <button
+            className="btn p-0 border-0 rounded-circle overflow-hidden"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            style={{ width: '40px', height: '40px' }}
+        >
+            <img
+            src="https://i.pravatar.cc/40"
+            alt="User Avatar"
+            className="w-100 h-100"
+            style={{ objectFit: 'cover' }}
+            />
+        </button>
+        <ul className="dropdown-menu dropdown-menu-dark">
+            <li>
+            <a className="dropdown-item" href="#"><i className="bi bi-0-circle"></i> Account</a>
+            </li>
+            <li><a className="dropdown-item" href="#">My Profile</a></li>
+            <li><a className="dropdown-item" href="#">Friends</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">New Roadmap</a></li>
+            <li><a className="dropdown-item" href="#">Roadmaps</a></li>
+            <li><hr className="dropdown-divider" /></li>
+            <li><a className="dropdown-item" href="#">Teams</a></li>
+            <li><a className="dropdown-item" onClick={onLogout}>Log out</a></li>
+        </ul>
+        </div>
+
+    }
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2 py-4">
             
@@ -17,25 +74,10 @@ export default function NavBar() {
                     <button className="btn btn-outline-light" type="submit">Search</button>
                 </form>
                 <div className="dropdown">
-                <button className="btn btn-warning  rounded-pill text-white" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown button
-                </button>
-                <ul className="dropdown-menu dropdown-menu-dark">
-                    <li><a className="dropdown-item" href="#"><i className="bi bi-0-circle"></i>Account</a></li>
-                    <li><a className="dropdown-item" href="#">My Profile</a></li>
-                    <li><a className="dropdown-item" href="#">Friends</a></li>
-                    <li><hr className="dropdown-divider" /></li>
-                    <li><a className="dropdown-item" href="#">New Roadmap</a></li>
-                    <li><a className="dropdown-item" href="#">Roadmaps</a></li>
-                    <li><hr className="dropdown-divider" /></li>
-                    <li><a className="dropdown-item" href="#">Teams</a></li>
-                    <li><a className="dropdown-item" href="#">Log out</a></li>
-                </ul>
+                {dropdown}
             </div>
             </div>
-
-            
-            </div>
+        </div>
            
 
         </nav>
