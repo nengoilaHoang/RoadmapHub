71d9f16da0eba84f787dedc24cd604eaddb5a1a9
KienLoveNH <kientri098@gmail.com>
2025-08-24 17:57:20 +0700

    create topic

--- frontend/src/pages/Roadmap/Nodes/Topic/Topic.jsx
+++ frontend/src/pages/Roadmap/Nodes/Topic/Topic.jsx
@@ -1,25 +1,10 @@
 import { Handle } from '@xyflow/react';
+import { ResizableNode } from '#Helps/ResizableNode.jsx';
 import './Topic.css';
-export default function Topic() {
+import { Rnd } from 'react-rnd';
+export default function Topic({ data, id,  selected }) {
   return (
-    <div className="custom-node">
-      <div>Topic name</div>
-
-      {/* TOP */}
-      <Handle type="source" position="top" id="top-source" style={{ left: '45%' }} />
-      <Handle type="target" position="top" id="top-target" style={{ left: '55%' }} />
-
-      {/* BOTTOM */}
-      <Handle type="source" position="bottom" id="bottom-source" style={{ left: '45%' }} />
-      <Handle type="target" position="bottom" id="bottom-target" style={{ left: '55%' }} />
-
-      {/* LEFT */}
-      <Handle type="source" position="left" id="left-source" style={{ top: '40%' }} />
-      <Handle type="target" position="left" id="left-target" style={{ top: '60%' }} />
-
-      {/* RIGHT */}
-      <Handle type="source" position="right" id="right-source" style={{ top: '40%' }} />
-      <Handle type="target" position="right" id="right-target" style={{ top: '60%' }} />
-    </div>
+    <ResizableNode data={data} id={id} selected = {selected} border ="2px solid #555" background ="#fff" borderRadius="8px" name="topic node"/>
+   
   );
 }
