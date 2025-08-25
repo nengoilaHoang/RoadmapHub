5b4f30bba5b420e79f45e950ce55833371377ea1
KienLoveNH <kientri098@gmail.com>
2025-08-25 17:19:17 +0700

    create service be

---
+++ backend/services/Roadmap.service.js
@@ -0,0 +1,17 @@
+import RoadmapDAO from '../daos/Roadmap.dao.js';
+class RoadmapService {
+    constructor(roadmapDAO) {
+        this.RoadmapDAO = roadmapDAO;
+    }
+
+    async createRoadmap(name, description, accountId) {
+        return await RoadmapDAO.createRoadmap(name, description, accountId);
+    }
+    async editRoadmap(name, description) {
+        return await RoadmapDAO.editRoadmap(name, description);
+    }
+    async deleteRoadmap(name) {
+        return await RoadmapDAO.deleteRoadmap(name);
+    }
+}
+export default new RoadmapService(RoadmapDAO)
