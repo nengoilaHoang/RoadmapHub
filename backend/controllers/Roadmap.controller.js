d52d2f007462594239b4a842eea81da175e9bc07
KienLoveNH <kientri098@gmail.com>
2025-08-28 10:15:59 +0700

    create roadmap

--- backend/controllers/Roadmap.controller.js
+++ backend/controllers/Roadmap.controller.js
@@ -1,12 +1,25 @@
 import RoadmapService from "../services/Roadmap.service.js";
+import { Buffer } from 'buffer';
 class RoadmapController {
 
     async createRoadmap(req, res) {
-        const { name, description } = req.body;
-        const accountId = req.user.id; // Assuming user ID is stored in req.user after authentication
+        const { name, description,userId } = req.body;
+        const realBuffer = Buffer.from(userId.data);
+        const accountId= realBuffer.toString();
+        console.log("Account ID:", accountId);
+        console.log("Name:", name);
+        console.log("Description:", description);
         try {
-            await RoadmapService.createRoadmap(name, description, accountId);
-            res.redirect(`/roadmap/view/:${name}`); // Redirect to the newly created roadmap view page
+            const responseCheck = await RoadmapService.checkRoadmap(name, accountId);
+            console.log("Response Check:", responseCheck);
+            if (!responseCheck.success) {
+                return res.json(responseCheck);
+            }
+            else{
+                await RoadmapService.createRoadmap(name, description, accountId);
+                res.redirect(`/roadmap/view/:${name}`); 
+            }
+            
         } catch (error) {
             console.error('Error creating roadmap:', error);
         }
@@ -19,6 +32,12 @@ class RoadmapController {
     async deleteRoadmap(req, res) {
         const { name } = req.params;
         await RoadmapService.deleteRoadmap(name);
+    }
+    async editNodeRoadmap(req, res) {
+        const { nodes,edges } = req.body;
+        console.log("nodes",nodes);
+        console.log("edges",edges);
+        await RoadmapService.editNodeRoadmap(nodes,edges);
     }
 }
 export default new RoadmapController(RoadmapService);
