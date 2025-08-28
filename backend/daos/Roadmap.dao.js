2489ac68b8d8a5032e9b426dcf8335739e116484
KienLoveNH <kientri098@gmail.com>
2025-08-28 10:16:30 +0700

    update roadmap

--- backend/daos/Roadmap.dao.js
+++ backend/daos/Roadmap.dao.js
@@ -1,6 +1,9 @@
 import db from '../utils/db.js'
+import connectDB from '../utils/dbmongo.js';
+import mongoose from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 import Roadmap from '../models/Roadmap.model.js';
+import NodeFactory from '../models/NodeFactory.model.js';
 class RoadmapDAO {
     async createRoadmap(name, description, accountId) {
         const roadmap = new Roadmap(Buffer.from(uuidv4().replace(/-/g, ''), 'hex'),accountId,"",name, description);
@@ -23,6 +26,36 @@ class RoadmapDAO {
             success: true,
             message: 'Delete roadmap successfully'
         }
+    }
+    async checkRoadmap(name,accountId){
+        const exit = await db('roadmap').where({name,accountId}).first();
+        if(exit){
+            return {
+                success:false,
+                message:"Name of roadmap already taken"
+            }
+        }
+        else {
+             return{
+                success:true,
+                message:"Roadmap already created successfully"
+            }
+        }
+    }
+    async editNodeRoadmap(nodes,edges) {
+        console.log("lll",nodes,edges); 
+        
+        await connectDB();
+        const Edge = mongoose.model("edges", new mongoose.Schema({
+        source: String,
+        target: String
+    },{ versionKey: false }));
+
+        const edge = new Edge({ source: "node1", target: "node2" });
+        await Edge.create(edge);
+
+        const Edges = await Edge.find();
+        console.log(edges);
     }
 }
 export default new RoadmapDAO();
