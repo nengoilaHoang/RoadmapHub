cd0a1a82e050190f74f255cbbc8684308536af1e
KienLoveNH <kientri098@gmail.com>
2025-08-29 13:34:08 +0700

    update roadmap dao

--- backend/daos/Roadmap.dao.js
+++ backend/daos/Roadmap.dao.js
@@ -4,17 +4,18 @@ import mongoose from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
 import Roadmap from '../models/Roadmap.model.js';
 import NodeFactory from '../models/NodeFactory.model.js';
+import geneUUID from '../Helps/genUUID.js'; 
 class RoadmapDAO {
     async createRoadmap(name, description, accountId) {
-        const roadmap = new Roadmap(Buffer.from(uuidv4().replace(/-/g, ''), 'hex'),accountId,"",name, description);
+        const roadmap = new Roadmap(geneUUID(),accountId,null,name, description,null, null, null);
         await db('roadmap').insert(roadmap);
         return {
                 success:true,
                 message:'Create roadmap successfully'
         }
     }
-    async editRoadmap(name, description) {
-        await db('roadmap').where({ name }).update({ name:name, description:description });
+    async editRoadmap(name, description,accountId,roadmapId) {
+        await db('roadmap').where({ accountId:accountId,id:roadmapId }).update({ name:name, description:description });
         return {
             success: true,
             message: 'Edit roadmap successfully'
@@ -68,6 +69,10 @@ class RoadmapDAO {
         .join('team', 'roadmap.teamId', 'team.id')
         .where('team.id', teamId)
         .select('roadmap.*');
+    }
+    async getRoadmapByName(accountId,name) {
+        const roadmap = await db('roadmap').where({accountId:accountId, name:name }).first();
+        return roadmap;
     }
 }
 export default new RoadmapDAO();
