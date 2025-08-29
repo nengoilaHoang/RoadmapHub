65d9a1cf9f35e9e85bcfb43608ed4acc2d9189bc
KienLoveNH <kientri098@gmail.com>
2025-08-29 13:40:25 +0700

    right bar title

--- frontend/src/components/Roadmap/Nodes/RightBar/RightBarTitle/RightBarTitle.jsx
+++ frontend/src/components/Roadmap/Nodes/RightBar/RightBarTitle/RightBarTitle.jsx
@@ -1,11 +1,11 @@
 import RightBarTop from "../RightBarTop/RightBarTop";
 
-export default function RightBarTitle({ selectedNode, onDeleteNode }) {
+export default function RightBarTitle({ selectedNode, onDeleteNode, onNodeChange }) {
     if (!selectedNode) return null;
     return (
         <div className={`rightbar ${selectedNode ? 'show' : ''}`}>
             <div className="rightbar-content">
-                <RightBarTop selectedNode={selectedNode} onDeleteNode={onDeleteNode} />
+                <RightBarTop selectedNode={selectedNode} onDeleteNode={onDeleteNode} onNodeChange={onNodeChange} />
            </div>
         </div>
     )
