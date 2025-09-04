4ea2ece60b70c49746ed4fc45bdab02830eb0384
Kien Mai <kientri098@gmail.com>
2025-09-04 20:55:55 +0700

    design horizontalline

--- frontend/src/components/Roadmap/Nodes/HorizontalLine/HorizontalLine.jsx
+++ frontend/src/components/Roadmap/Nodes/HorizontalLine/HorizontalLine.jsx
@@ -1,14 +1,11 @@
-import { ResizableNode } from '#Helps/ResizableNode.jsx';
-export default function HozicontalLine({ data, id, selected }) {
+import { Line } from '#components/Roadmap/Nodes/Node/Line.jsx';
+export default function HozicontalLine({ data, id, selected,type }) {
   return (
-    <ResizableNode
+    <Line
       data={data}
       id={id}
       selected={selected}
-      border="2px solid #555"
-      background="#fff"
-      borderRadius="8px"
-      name="horizontal line node"
+      type={type}
     />
   );
 }
