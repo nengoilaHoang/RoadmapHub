fb2e4bbd57685970409e003ffd7230c78f90fa9a
Kien Mai <kientri098@gmail.com>
2025-09-04 20:52:00 +0700

    delete strict mode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { GoogleOAuthProvider } from '@react-oauth/google';
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
         <GoogleOAuthProvider clientId = '63811731752-1j2m5pdiontrrpvl9htjr17p0pp71aq1.apps.googleusercontent.com'>
             <App />
         </GoogleOAuthProvider>
-    </StrictMode>
     
 )
