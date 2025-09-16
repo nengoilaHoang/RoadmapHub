d7561f12780312fd6fc02c7c434b263f3015daba
Kien Mai <kientri098@gmail.com>
2025-09-16 21:25:50 +0700

    add student form

---
+++ frontend/src/components/Classroom/ManageStudent/AddStudentForm/AddStudentForm.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./AddStudentForm.css";
+import api from '../../../../utils/api.js'
+import AlertError from '#components/SignUp/AlertError.jsx';
+
+export default function AddStudentForm(props) {
+  const {classroomId} = props;
+  const [email, setEmail] = useState("");
+  const [error,setError] = useState("");
+  const [alertEmail,setAlertEmail] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!email) return;
+    const response = await api.post('/studentclassrooms/add', { email: email,classroomId:classroomId },{
+      withCredentials: true
+    });
+    console.log(response.data);
+    if (response.data.success === false) {
+        setError(response.data.message);
+        setAlertEmail(true);
+    }
+    else setAlertEmail(false);
+    setEmail("");
+  };
+
+  return (
+    <form className="card form" onSubmit={handleSubmit}>
+      <h2>Add students</h2>
+      <input
+        type="email"
+        placeholder="Enter email..."
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="input"
+        required
+      />
+      {alertEmail&&<AlertError content={error}/>}
+      <button onClick={handleSubmit} className="btn send">Send</button>
+    </form>
+  );
+}
