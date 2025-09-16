d529b3dc50c560a50b29bb4d3f74c671a94f1300
Kien Mai <kientri098@gmail.com>
2025-09-16 21:41:39 +0700

    update forum class

---
+++ frontend/src/components/Classroom/ForumClass/ForumClass.jsx
@@ -0,0 +1,81 @@
+import React, { useState,useEffect } from "react";
+import Post from "#components/Classroom/ForumClass/Post/Post.jsx";
+import ItemPost from "#components/Classroom/ForumClass/ItemPost/ItemPost";
+
+import api from '#utils/api.js'
+export default function ForumClass(props) {
+  const {classroomId}=props;
+  const [posts,setPost]=useState([]);
+   async function getPosts(){
+            const response = await api.get('/posts/getPosts',{
+                withCredentials: true,
+                params:{classroomId:classroomId}
+            })
+            setPost(response.data);
+           
+        }
+  useEffect(()=>{
+       
+        getPosts();
+    },[])
+  const handlePost = async (content) => {
+    const response = await api.post('/posts/create',{
+      classroomId:classroomId,
+      content:content
+    },{ withCredentials: true})
+    // console.log(response);
+    await getPosts();
+  };
+  const handleEditPost = async(content,id)=>{
+        const response = await api.put(`/posts/update/${id}`,{
+            content:content
+        })
+        await getPosts();
+    }
+  const handleDeletePost=async(id)=>{
+        const response = await api.delete(`/posts/delete/${id}`);
+        await getPosts();
+  }
+  const handleComment = async(postId,content)=>{
+     const response = await api.post('/comments/create',{
+      classroomId:classroomId,
+      postId:postId,
+      content:content
+    },{ withCredentials: true})
+    // console.log(response);
+    await getPosts();
+  }
+  const handleEditComment = async(content,id)=>{
+        const response = await api.put(`/comments/update/${id}`,{
+            content:content
+        })
+        await getPosts();
+    }
+  const handleDeleteComment = async(id)=>{
+        const response = await api.delete(`/comments/delete/${id}`);
+        await getPosts();
+  }
+
+  return (
+    <div className="container mt-4">
+      <Post
+        onCancel={() => console.log("Cancel")}
+        onPost={handlePost}
+      />
+      {posts.map((post) => (
+        <ItemPost key={post.postId} 
+                  post={post.post} 
+                  comments={post.comments} 
+                  handleEditPost={handleEditPost} 
+                  handleDeletePost={handleDeletePost} 
+                  handleComment = {handleComment}
+                  handleEditComment = {handleEditComment}
+                  handleDeleteComment = {handleDeleteComment}
+        />
+         
+    ))}
+
+
+    </div>
+  );
+}
