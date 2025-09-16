aa2b1aca96d8417603c8d2b2589d1dc92df757b4
Kien Mai <kientri098@gmail.com>
2025-09-16 21:22:02 +0700

    update index

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ import profileRoute from './routes/profile.route.js'
 import roadmapRoutes from './routes/Roadmap.route.js'
 import teamRoutes from './routes/team.route.js'
 import friendRoutes from './routes/friend.route.js'
+import classroomRoutes from './routes/classroom.route.js'
+import studentclassroomRoutes from './routes/studentclassroom.route.js'
+import postRoutes from './routes/post.route.js'
+import commentRoutes from './routes/comment.route.js';
 import cors from 'cors'
 import authenticate from './middlewares/AuthMiddleware.js';
 import cookieParser from "cookie-parser";
@@ -29,6 +33,10 @@ app.use('/api/auth', auRoutes);
 app.use('/api/roadmaps', roadmapRoutes);
 app.use('/api/teams', teamRoutes);
 app.use('/api/friends', friendRoutes);
+app.use('/api/classrooms',classroomRoutes);
+app.use('/api/studentclassrooms',studentclassroomRoutes);
+app.use('/api/posts',postRoutes)
+app.use('/api/comments', commentRoutes);
 app.listen(process.env.PORT, () => {
     console.log(`Server is running at http://localhost:${process.env.PORT}`)
 });
