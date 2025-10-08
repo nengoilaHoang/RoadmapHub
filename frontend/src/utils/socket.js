ecb55472c5bf6f6cae716877d90e2f660761ed27
nengoilaHoang <levanviethoang04@gmail.com>
2025-10-08 20:21:07 +0700

    merge code

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,6 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", { withCredentials: true });
+// const socket = io("http://localhost:5000", { withCredentials: true });
+const socket = io("https://santo-nonductile-agelessly.ngrok-free.dev", { withCredentials: true });
 
 export default socket;
