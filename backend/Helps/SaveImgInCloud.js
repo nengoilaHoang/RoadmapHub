a7c74b14d0a708341b98fa97144da7757876ddfd
nguyentruong0904 <nguyentruongpro19@gmail.com>
2025-10-11 10:17:56 +0700

    fix: Update Cloudinary configuration to use correct environment variable names

--- backend/Helps/SaveImgInCloud.js
+++ backend/Helps/SaveImgInCloud.js
@@ -6,9 +6,9 @@ dotenv.config();
 
 // Cấu hình Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Hàm upload
